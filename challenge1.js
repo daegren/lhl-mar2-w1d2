5b98728a1c976ec6bbe10e067b7b62adec48755d
David Mills <dave@1morepx.com>
2020-03-03 11:13:40 -0500

    Disregarding floating point numbers

--- challenge1.js
+++ challenge1.js
@@ -4,13 +4,21 @@ const args = process.argv.slice(2);
 
 // console.log("args", args);
 
+// Whole numbers % 1 will always be 0
+const isWholeNumber = function(number) {
+  return number % 1 === 0;
+};
+
 const sum = function(numbers) {
   let total = 0;
 
   for (let i = 0; i < numbers.length; i++) {
     const item = args[i];
-    const number = parseInt(item, 10);
-    total += number;
+    const number = parseFloat(item, 10);
+
+    if (isWholeNumber(number)) {
+      total += number;
+    }
   }
 
   return total;
