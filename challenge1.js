b4b14ec9d2341ef7a2d7685b5609f5b54805c1b6
David Mills <dave@1morepx.com>
2020-03-03 11:04:48 -0500

    Refacored out a sum function

--- challenge1.js
+++ challenge1.js
@@ -4,13 +4,16 @@ const args = process.argv.slice(2);
 
 // console.log("args", args);
 
-let total = 0;
-for (let i = 0; i < args.length; i++) {
-  const item = args[i];
-  // console.log("Item", item, typeof item);
-  const number = parseInt(item, 10);
-  // console.log("number", number, typeof number);
-  total += number;
-}
+const sum = function(numbers) {
+  let total = 0;
 
-console.log("Total:", total);
+  for (let i = 0; i < numbers.length; i++) {
+    const item = args[i];
+    const number = parseInt(item, 10);
+    total += number;
+  }
+
+  return total;
+};
+
+console.log("Total:", sum(args));
